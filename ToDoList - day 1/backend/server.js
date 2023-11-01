175386e3c8acbefbf4a240f3ba0c45060c8ee177
Parv-gugnani <9765.parv@gmail.com>
2023-11-01 15:50:05 +0530

    tofo

--- ToDoList - day 1/backend/server.js
+++ ToDoList - day 1/backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const moongoose = require("mongoose");
-const cars = require("cars");
-const TodoModel = require("./models/todolist");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const TodoModel = require("./models/todoList");
 
 var app = express();
 app.use(cors());
@@ -13,39 +13,41 @@ mongoose.connection.on("error", (error) => {
   console.error("MongoDB connection error:", error);
 });
 
-app.get("/getTodoList", (req, res) => { 
-    TodoModel.find({}) 
-        .then((todoList) => res.json(todoList)) 
-        .catch((err) => res.json(err)) 
-}); 
-
-app.post("./addToDoList", (req, res) => {
-    TodoModel.create({
-        task: req.body.task,
-        status: req.body.status,
-        deadline:req.body.deadline,
-    }) 
-    .then((todo)=> res.json(todo)).catch((err)=> res.json(err);
+app.get("/getTodoList", (req, res) => {
+  TodoModel.find({})
+    .then((todoList) => res.json(todoList))
+    .catch((err) => res.json(err));
 });
 
-app.post("/updateTodoList", (req, res) => {
-    const id = req.params.id;
-    const updateData={
-        task: req.body.task, 
-        status: req.body.status, 
-        deadline: req.body.deadline,  
-    }
-    TodoModel.findByIdAndUpdate(id, updateData) 
-    .then((todo) => res.json(todo)) 
-    .catch((err) => res.json(err)); 
-})
+app.post("/addTodoList", (req, res) => {
+  TodoModel.create({
+    task: req.body.task,
+    status: req.body.status,
+    deadline: req.body.deadline,
+  })
+    .then((todo) => res.json(todo))
+    .catch((err) => res.json(err));
+});
 
-app.delete('./deleteToDoList/:id', (req,res)=>{
-    const id =req.params.id;
-    TodoModel.findByIdAndDelete({_id:id})
-    .then((todo)=>res.json(todo)).catch((err) =>{res.json(err)})
-})
+app.post("/updateTodoList/:id", (req, res) => {
+  const id = req.params.id;
+  const updateData = {
+    task: req.body.task,
+    status: req.body.status,
+    deadline: req.body.deadline,
+  };
+  TodoModel.findByIdAndUpdate(id, updateData)
+    .then((todo) => res.json(todo))
+    .catch((err) => res.json(err));
+});
 
-app.listen(3001,()=>{
-    console.log(`server is running on http://localhost:3001/`);
-})
+app.delete("/deleteTodoList/:id", (req, res) => {
+  const id = req.params.id;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then((todo) => res.json(todo))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server running on 3001");
+});
