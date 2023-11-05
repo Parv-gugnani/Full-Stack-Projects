f0f48e2732bbef3eeb287cad9b546f4c5ffffbf7
Parv-gugnani <9765.parv@gmail.com>
2023-11-05 16:19:28 +0530

    home

--- Ecommerce - day 4/ecomrce/src/Components/index.js
+++ Ecommerce - day 4/ecomrce/src/Components/index.js
@@ -1,2 +1,8 @@
-export { BillCardForCart } from "./CartCards/BillCardForCart";
+export { Navbar } from "./Navbar/Navbar";
+export { FilterSidebar } from "./FilterSidebar/FilterSidebar";
+export { ProductCard } from "./ProductCard/ProductCard";
+export { Categories } from "./Categories/Categories";
 export { ProductCardForCart } from "./CartCards/ProductCardForCart";
+export { BillCardForCart } from "./CartCards/BillCardForCart";
+export { HamburgurMenuList } from "./HamburgurMenu/HamburgurMenu";
+export { Footer } from "./Footer/Footer";
