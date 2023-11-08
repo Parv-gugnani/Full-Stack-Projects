da1f306a6bc1a62e9a260b96a3eed34fb0550a31
Parv-gugnani <9765.parv@gmail.com>
2023-11-08 09:58:12 +0530

    auth

--- Mern Auth day-4/server/controllers/authController.js
+++ Mern Auth day-4/server/controllers/authController.js
@@ -1,11 +1,38 @@
+const User = require("../models/user");
+
 const test = (req, res) => {
   res.json("test is working");
 };
 
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
   try {
     const { name, email, pasword } = req.body;
-  } catch (error) {}
+    //
+    if (!name) {
+      return res.json({
+        error: "name is required",
+      });
+    }
+    // password
+    if (!password || !password.length < 6) {
+      return res.json({
+        error: "password must be at least 6 characters",
+      });
+    }
+    // check
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "email is taken already",
+      });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { test, registerUser };
