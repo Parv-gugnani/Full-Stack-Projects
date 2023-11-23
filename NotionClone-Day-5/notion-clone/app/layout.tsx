a8ea68ddf33d04fa72b6033ec9b0c14a01666eca
Parv-gugnani <9765.parv@gmail.com>
2023-11-23 22:39:25 +0530

    public

--- NotionClone-Day-5/notion-clone/app/layout.tsx
+++ NotionClone-Day-5/notion-clone/app/layout.tsx
@@ -7,7 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "NoteWave",
   description: "Help you to keep Structure in your Life",
-  icons: ["logo.png"],
+
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="logo.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
