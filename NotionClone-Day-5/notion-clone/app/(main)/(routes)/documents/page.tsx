0ca3c9c452ff945d9530a38ab69d65368afc141e
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 18:00:51 +0530

    welcome

--- NotionClone-Day-5/notion-clone/app/(main)/(routes)/documents/page.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
-  return <div className="p-20">Documents Page</div>;
+  const { user } = useUser();
+
+  //
+  //
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="Empty"
+        width="300"
+        height="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="Empty"
+        width="300"
+        height="300"
+        className="hidden dark:block"
+      />
+      {/* nice way to write first name */}
+      <h2 className="text-lg font-medium">
+        Ride the Wave with {user?.firstName}!
+      </h2>
+
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
