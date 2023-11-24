f2c70ac40c8f28540b1d0e5745f55cf0e44d2679
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 13:28:39 +0530

    ath completed

--- NotionClone-Day-5/notion-clone/app/(marketing)/_components/heading.tsx
+++ NotionClone-Day-5/notion-clone/app/(marketing)/_components/heading.tsx
@@ -30,7 +30,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Wave
+            Dive In
             <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
