a79be21c15076f785cb23c07a8549be173e254af
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 14:37:59 +0530

    redirect working

---
+++ NotionClone-Day-5/notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+  return <div>This is a Protected DocumentsPage</div>;
+};
+
+export default DocumentsPage;
---
+++ NotionClone-Day-5/notion-clone/app/(main)/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    redirect("/");
+  }
+  //
+  return <div>{children}</div>;
+};
+
+export default MainLayout;
