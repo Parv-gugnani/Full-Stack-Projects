c03727906b8cd76858c6728a9506e8cb159c2a1b
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 10:00:31 +0530

    theme

--- NotionClone-Day-5/notion-clone/app/layout.tsx
+++ NotionClone-Day-5/notion-clone/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,9 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <link rel="icon" href="logo.png" />
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="notewave-theme-2"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
