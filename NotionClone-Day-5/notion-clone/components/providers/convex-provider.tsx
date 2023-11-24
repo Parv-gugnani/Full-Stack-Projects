4cb115eedfb6323157488bfb165106ceff12fad6
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 11:03:09 +0530

    fi

---
+++ NotionClone-Day-5/notion-clone/components/providers/convex-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk useAuth={useauth}></ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
