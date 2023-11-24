7efd817a5ba6fa49b27fd8e612cc27d4ea3ae2b2
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 19:37:42 +0530

    usersetting

--- NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
@@ -1,5 +1,34 @@
 "use client";
+import { ChevronsLeftRight } from "lucide-react";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { useUser } from "@clerk/clerk-react";
 
 export const UserItem = () => {
-  return <div>User Item</div>;
+  const { user } = useUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span>{user?.firstName}&apos;s Wave</span>
+          </div>
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
+        </div>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
