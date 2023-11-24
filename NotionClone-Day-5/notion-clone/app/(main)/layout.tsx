db538ecf15eaac6a8d5e29e933deb63273d478b5
Parv-gugnani <9765.parv@gmail.com>
2023-11-24 16:02:29 +0530

    sudden back

--- NotionClone-Day-5/notion-clone/app/(main)/layout.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/layout.tsx
@@ -21,7 +21,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }
   //
   return (
-    <div className="h-full flex dark:bg-[#1f1f1f]">
+    <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
