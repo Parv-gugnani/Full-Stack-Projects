4667db6f663470100a5ce6aca0025283f1641fd6
Parv-gugnani <9765.parv@gmail.com>
2023-11-25 10:32:59 +0530

    user item

--- NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
@@ -1,15 +1,17 @@
 "use client";
+
 import { ChevronsLeftRight } from "lucide-react";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { useUser, SignOutButton } from "@clerk/clerk-react";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { useUser } from "@clerk/clerk-react";
-import { SignOutButton } from "@clerk/clerk-react";
+} from "@/components/ui/dropdown-menu";
 
 export const UserItem = () => {
   const { user } = useUser();
@@ -26,12 +28,43 @@ export const UserItem = () => {
               <AvatarImage src={user?.imageUrl} />
             </Avatar>
             <span className="text-start font-medium line-clamp-1">
-              {user?.firstName}&apos;s Wave
+              {user?.fullName}&apos;s Jotion
             </span>
           </div>
           <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
         </div>
       </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-80"
+        align="start"
+        alignOffset={11}
+        forceMount
+      >
+        <div className="flex flex-col space-y-4 p-2">
+          <p className="text-xs font-medium leading-none text-muted-foreground">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+          <div className="flex items-center gap-x-2">
+            <div className="rounded-md bg-secondary p-1">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm line-clamp-1">
+                {user?.firstName}&apos;s Wave
+              </p>
+            </div>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          asChild
+          className="w-full cursor-pointer text-muted-foreground"
+        >
+          <SignOutButton>Log out</SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
