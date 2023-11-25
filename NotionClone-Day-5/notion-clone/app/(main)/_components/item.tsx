ee8365c7ccefd9cb1e10ff9b069f8f2357d2bede
Parv-gugnani <9765.parv@gmail.com>
2023-11-25 16:08:52 +0530

    note

--- NotionClone-Day-5/notion-clone/app/(main)/_components/item.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/item.tsx
@@ -58,8 +58,8 @@ export const Item = ({
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd>
-          <span></span>
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
         </kbd>
       )}
     </div>
