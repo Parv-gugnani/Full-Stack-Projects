33e0756425b5d1497a28cce2e4bc623a14d24efd
Parv-gugnani <9765.parv@gmail.com>
2023-11-26 14:07:11 +0530

    archive

--- NotionClone-Day-5/notion-clone/convex/documents.ts
+++ NotionClone-Day-5/notion-clone/convex/documents.ts
@@ -2,6 +2,30 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    return document;
+  },
+});
 export const getSidebar = query({
   args: {
     parentDocument: v.optional(v.id("documents")),
