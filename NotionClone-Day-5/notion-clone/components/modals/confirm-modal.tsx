929eada687cb597915ce4d9a2ae9bcc56dfa4bc3
Parv-gugnani <9765.parv@gmail.com>
2023-11-27 15:43:44 +0530

    confirm modal

---
+++ NotionClone-Day-5/notion-clone/components/modals/confirm-modal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTrigger,
+  AlertDialogTitle,
+  AlertDialogHeader,
+  AlertDialogFooter,
+} from "@radix-ui/react-alert-dialog";
+
+export const ConfirmModal = {
+  children,
+  onConfirm}:ConfirmModalProps
+};
