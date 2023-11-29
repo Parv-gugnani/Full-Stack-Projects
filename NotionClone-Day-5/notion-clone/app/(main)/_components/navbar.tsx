a73bdf5d2accb0e50b8a0e0fc80b6b88e0d66e0b
Parv-gugnani <9765.parv@gmail.com>
2023-11-29 18:43:34 +0530

    title

--- NotionClone-Day-5/notion-clone/app/(main)/_components/navbar.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import { Title } from "./title";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -33,7 +34,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between w-full">Navbar</div>
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
       </nav>
     </>
   );
---
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/title.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { useMutation } from "convex/react";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface TitleProps {
+  initialData: Doc<"documents">;
+}
+
+export const Title = ({ initialData }: TitleProps) => {
+  const update = useMutation(api.documents.update);
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input className="h-7 px-2 focus-visible:ring-transparent" />
+      ) : (
+        <Button
+          onClick={() => {}}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData?.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
