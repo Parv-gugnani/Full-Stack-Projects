6cda0dccf388bd2b36b0ea711b2442630217c99f
Parv-gugnani <9765.parv@gmail.com>
2023-11-29 22:18:56 +0530

    navbar completed

--- NotionClone-Day-5/notion-clone/app/(main)/_components/navbar.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/navbar.tsx
@@ -18,7 +18,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   });
 
   if (document === undefined) {
-    return <p>Loading...</p>;
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+        <Title.skeleton />
+      </nav>
+    );
   }
 
   if (document === null) {
