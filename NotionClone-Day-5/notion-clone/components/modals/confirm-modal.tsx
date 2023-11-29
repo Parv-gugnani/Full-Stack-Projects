29102b6447b64482f714076ed48bbae0d609550d
Parv-gugnani <9765.parv@gmail.com>
2023-11-29 15:01:42 +0530

    confirm fix

--- NotionClone-Day-5/notion-clone/components/modals/confirm-modal.tsx
+++ NotionClone-Day-5/notion-clone/components/modals/confirm-modal.tsx
@@ -24,6 +24,7 @@ export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
     e.stopPropagation();
     onConfirm();
   };
+
   return (
     <AlertDialog>
       <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
@@ -31,16 +32,16 @@ export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you Sure about that ?</AlertDialogTitle>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone
+            This action cannot be undone.
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
             Cancel
           </AlertDialogCancel>
-          <AlertDialogAction>Confirm</AlertDialogAction>
+          <AlertDialogAction onClick={handleConfirm}>Confirm</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
