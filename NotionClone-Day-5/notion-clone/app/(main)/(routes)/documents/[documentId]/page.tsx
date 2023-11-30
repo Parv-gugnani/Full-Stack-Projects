7cc643fc85c7b7eb7d35a0f0b34e11206b12c34d
Parv-gugnani <9765.parv@gmail.com>
2023-11-30 14:34:32 +0530

    document id

--- NotionClone-Day-5/notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,32 @@
-const DocumentIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface DocumentIdPage {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPage) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    <div>Loading....</div>;
+  }
+
+  if (document === null) {
+    return (
+      <div className="pb-40">
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto">DocumentId</div>
+      </div>
+    );
+  }
+
   return <div>Document Id</div>;
 };
 
