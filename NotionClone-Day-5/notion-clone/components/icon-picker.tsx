949ccc13089b23ec3183cb00a82b245f754bfe5f
Parv-gugnani <9765.parv@gmail.com>
2023-11-30 15:01:47 +0530

    document id

---
+++ NotionClone-Day-5/notion-clone/components/icon-picker.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Theme } from "emoji-picker-react";
+import EmojiPicker from "emoji-picker-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import { useTheme } from "next-themes";
+
+interface IconPickerProps {
+  onChange: (icon: string) => void;
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export const IconPicker = ({
+  onChange,
+  children,
+  asChild,
+}: IconPickerProps) => {
+  const { resolvedTheme } = useTheme();
+  const currentTheme = (resolvedTheme || "Light") as keyof typeof themeMap;
+
+  const themeMap = {
+    dark: Theme.DARK,
+    light: Theme.LIGHT,
+  };
+
+  const theme = themeMap[currentTheme];
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
+      <PopoverContent className="p-0 w-full border-none shadow-none">
+        <EmojiPicker
+          height={350}
+          theme={theme}
+          onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
--- NotionClone-Day-5/notion-clone/components/toolbar.tsx
+++ NotionClone-Day-5/notion-clone/components/toolbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -11,7 +12,13 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
-        <div className="flex items-center gap-x-2 group/icon pt-6"></div>
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={()=>()}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+        </div>
       )}
     </div>
   );
