48b652381a4192b70f23002b825f0374178350db
Parv-gugnani <9765.parv@gmail.com>
2023-11-30 16:48:58 +0530

    toolbar

--- NotionClone-Day-5/notion-clone/components/toolbar.tsx
+++ NotionClone-Day-5/notion-clone/components/toolbar.tsx
@@ -2,6 +2,8 @@
 
 import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
+import { Button } from "@/components/ui/button";
+import { Smile, X } from "lucide-react";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -13,13 +15,38 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6">
-          <IconPicker onChange={()=>()}>
+          <IconPicker onChange={() => {}}>
             <p className="text-6xl hover:opacity-75 transition">
               {initialData.icon}
             </p>
           </IconPicker>
+          <Button
+            onClick={() => {}}
+            className="rounded-full opacity-0 group-hover/ icon:opacity-100  transition text-muted-foreground text-xs"
+            size="icon"
+            variant="outline"
+          >
+            <X className="h-4 w-4" />
+          </Button>
         </div>
       )}
+      {!!initialData.icon && preview && (
+        <p className="text-6xl pt-6">{initialData.icon}</p>
+      )}
+      <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+        {!initialData.icon && !preview && (
+          <IconPicker asChild onChange={() => {}}>
+            <Button
+              className="text-muted-foreground text-xs"
+              variant="outline"
+              size="sm"
+            >
+              <Smile className="h-4 w-4 mr-2" />
+              Add icon
+            </Button>
+          </IconPicker>
+        )}
+      </div>
     </div>
   );
 };
