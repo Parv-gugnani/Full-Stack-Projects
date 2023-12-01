19f10c92027b10c78297d3f0e533aa83c0d1868a
Parv-gugnani <9765.parv@gmail.com>
2023-12-01 14:48:43 +0530

    remove icon

--- NotionClone-Day-5/notion-clone/components/toolbar.tsx
+++ NotionClone-Day-5/notion-clone/components/toolbar.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { ElementRef, useRef, useState } from "react";
+import { ImageIcon, Smile, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import TextareaAutosize from "react-textarea-autosize";
-import { Doc } from "@/convex/_generated/dataModel";
-import { ImageIcon, Smile, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { IconPicker } from "./icon-picker";
-import { api } from "@/convex/_generated/api";
+
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { Doc } from "@/convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+
+import { IconPicker } from "./icon-picker";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -21,6 +23,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   const [value, setValue] = useState(initialData.title);
 
   const update = useMutation(api.documents.update);
+  // remmove  icoon  fix
   const removeIcon = useMutation(api.documents.removeIcon);
 
   const coverImage = useCoverImage();
