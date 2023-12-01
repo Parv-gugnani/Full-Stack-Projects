87937fe11c472baab94bb1e55accf122bce91087
Parv-gugnani <9765.parv@gmail.com>
2023-12-01 15:31:29 +0530

    coverimg modal

--- NotionClone-Day-5/notion-clone/components/providers/modal-provider.tsx
+++ NotionClone-Day-5/notion-clone/components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { SettingModal } from "../modals/settings-modal";
+import { ConverImageModal } from "../modals/cover-image-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
   return (
     <>
       <SettingModal />
+      <ConverImageModal />
     </>
   );
 };
