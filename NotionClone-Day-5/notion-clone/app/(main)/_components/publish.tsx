59d0e0ac704c506c3fd6aceac8c089db33fcab07
Parv-gugnani <9765.parv@gmail.com>
2023-12-02 18:28:52 +0530

    ispublish

--- NotionClone-Day-5/notion-clone/app/(main)/_components/publish.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/publish.tsx
@@ -1,11 +1,104 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
-import { Popover, PopoverTrigger, pop } from "@radix-ui/react-popover";
+import { Check, Copy, Globe } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 interface PublishProps {
   initialData: Doc<"documents">;
 }
 
 export const Publish = ({ initialData }: PublishProps) => {
-  return <div>Publish </div>;
+  const origin = useOrigin();
+  const update = useMutation(api.documents.update);
+  const [copied, setIsCopied] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const url = `${origin}/preview/${initialData._id}`;
+  const onPublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: true,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Publishing...",
+      success: "Note published",
+      error: "failed to publish",
+    });
+  };
+
+  //
+  //
+  //
+
+  const onUnpublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({
+      id: initialData._id,
+      isPublished: false,
+    }).finally(() => setIsSubmitting(false));
+
+    toast.promise(promise, {
+      loading: "Unpublishing...",
+      success: "Note unpublished",
+      error: "Failed to unpublish note.",
+    });
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(url);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size="sm" variant="ghost">
+          Publish{" "}
+          {initialData.isPublished && (
+            <Globe className="text-sky-500 w-4 h-4 ml-2" />
+          )}
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
+        {initialData.isPublished ? (
+          <div>Published</div>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <Globe className="h-8 w-8 text-muted-foreground mb-2" />
+
+            <p className="text-sm font-medium mb-2">Publish this note</p>
+            <span className="text-xs text-muted-foreground mb-4">
+              Share your work with others
+            </span>
+            <Button
+              disabled={isSubmitting}
+              onClick={onPublish}
+              className="w-full text-xs"
+              size="sm"
+            >
+              Publish
+            </Button>
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
 };
