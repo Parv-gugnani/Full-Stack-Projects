3514eddd9a3339f49793a89fc8feeb1d0f66b131
Parv-gugnani <9765.parv@gmail.com>
2023-12-02 18:36:07 +0530

    ispublish

--- NotionClone-Day-5/notion-clone/app/(main)/_components/publish.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/publish.tsx
@@ -79,7 +79,31 @@ export const Publish = ({ initialData }: PublishProps) => {
 
       <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
         {initialData.isPublished ? (
-          <div>Published</div>
+          <div className="space-y-4">
+            <div className="flex items-center gap-x-2">
+              <Globe className="text-sky-500 animate-pulse h-4 w-4" />
+              <p className="text-xs font-medium text-sky-500">
+                This note is live on web
+              </p>
+            </div>
+            <div className="flex items-center">
+              <input
+                className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+                value={url}
+              />
+              <Button
+                onClick={onCopy}
+                disabled={copied}
+                className="h-8 rounded-l-none"
+              >
+                {copied ? (
+                  <Check className="h-4 w-4" />
+                ) : (
+                  <Copy className="h-4 w-4" />
+                )}
+              </Button>
+            </div>
+          </div>
         ) : (
           <div className="flex flex-col items-center justify-center">
             <Globe className="h-8 w-8 text-muted-foreground mb-2" />
