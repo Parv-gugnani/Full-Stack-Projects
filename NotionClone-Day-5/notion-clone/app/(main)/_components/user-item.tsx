c739534dce9c3e3b835ff10cd1fba1dc5fadb1e2
Parv-gugnani <9765.parv@gmail.com>
2023-12-04 13:42:42 +0530

    day 5 completed

--- NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
+++ NotionClone-Day-5/notion-clone/app/(main)/_components/user-item.tsx
@@ -28,7 +28,7 @@ export const UserItem = () => {
               <AvatarImage src={user?.imageUrl} />
             </Avatar>
             <span className="text-start font-medium line-clamp-1">
-              {user?.fullName}&apos;s Jotion
+              {user?.fullName}&apos;s Wave
             </span>
           </div>
           <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
