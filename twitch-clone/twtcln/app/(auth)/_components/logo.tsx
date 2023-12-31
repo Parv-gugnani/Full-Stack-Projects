4fc1b33b98c60f9475c5a12c97c71dac349f7244
Parv-gugnani <9765.parv@gmail.com>
2023-12-31 13:21:19 +0530

    logo

--- twitch-clone/twtcln/app/(auth)/_components/logo.tsx
+++ twitch-clone/twtcln/app/(auth)/_components/logo.tsx
@@ -1,3 +1,21 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-1">
+        <Image src="./logo.svg" alt="GameHub" height="80" width="80" />
+      </div>
+      <div className="flex flex-col items-center">
+        <p>Twitch Clone</p>
+      </div>
+    </div>
+  );
+};
