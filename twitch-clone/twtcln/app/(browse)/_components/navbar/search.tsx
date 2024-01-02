7c5bc97c931ae760530b70fd8600bb37ff98cc5d
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 09:51:20 +0530

    url

--- twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
@@ -7,14 +7,39 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 export const Search = () => {
+  const router = useRouter();
+  const [value, setvalue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+    router.push(url);
+  };
   return (
     <form
       onSubmit={() => {}}
       className="relative w-full lg:w-[400px] flex items-center"
     >
-      <Input placeholder="search..." />
-      <Button>
-        <SearchIcon />
+      <Input
+        placeholder="search..."
+        className="rounded-r-none focus-visible:*:ring-0 focus-visible:ring-transparent  focus-visible:ring-offset-0"
+      />
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <SearchIcon className="h-5 w-5 text-muted-foreground" />
       </Button>
     </form>
   );
