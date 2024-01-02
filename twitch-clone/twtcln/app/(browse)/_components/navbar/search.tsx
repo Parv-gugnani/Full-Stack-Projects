e89a02f94f281e2905d9c223639fafb68854ff0e
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 09:36:17 +0530

    search bar

--- twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
@@ -1,4 +1,21 @@
 "use client";
-export const Search = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const Search = () => {
+  return (
+    <form
+      onSubmit={() => {}}
+      className="relative w-full lg:w-[400px] flex items-center"
+    >
+      <Input placeholder="search..." />
+      <Button>
+        <SearchIcon />
+      </Button>
+    </form>
+  );
 };
