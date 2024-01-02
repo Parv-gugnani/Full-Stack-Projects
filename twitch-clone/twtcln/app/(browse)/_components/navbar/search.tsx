114a0003dd9eb24359d43c878fe8f6555e117037
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 09:58:53 +0530

    serch url

--- twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/navbar/search.tsx
@@ -17,7 +17,7 @@ export const Search = () => {
 
     const url = qs.stringifyUrl(
       {
-        url: "/",
+        url: "/search",
         query: { term: value },
       },
       { skipEmptyString: true }
