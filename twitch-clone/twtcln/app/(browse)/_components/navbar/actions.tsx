8e47ec0f3f2523c4ea3949574fb8677158c2ffbb
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 12:17:07 +0530

    actions

--- twitch-clone/twtcln/app/(browse)/_components/navbar/actions.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/navbar/actions.tsx
@@ -1,20 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { SignInButton, currentUser } from "@clerk/nextjs";
 import Link from "next/link";
 import { Clapperboard } from "lucide-react";
+import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export const Actions = async () => {
   const user = await currentUser();
 
-  //
   return (
-    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-2">
+    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
       {!user && (
         <SignInButton>
-          <Button>Login</Button>
+          <Button size="sm" variant="primary">
+            Login
+          </Button>
         </SignInButton>
       )}
-      {/* user  */}
       {!!user && (
         <div className="flex items-center gap-x-4">
           <Button
@@ -24,10 +24,11 @@ export const Actions = async () => {
             asChild
           >
             <Link href={`/u/${user.username}`}>
-              <Clapperboard />
-              <span>DashBoard</span>
+              <Clapperboard className="h-5 w-5 lg:mr-2" />
+              <span className="hidden lg:block">Dashboard</span>
             </Link>
           </Button>
+          <UserButton afterSignOutUrl="/" />
         </div>
       )}
     </div>
