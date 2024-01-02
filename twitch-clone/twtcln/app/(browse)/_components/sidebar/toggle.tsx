f69b3cc603b28189d9dadea2faca8cb016b332a0
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 17:23:44 +0530

    sidebar toogle

--- twitch-clone/twtcln/app/(browse)/_components/sidebar/toggle.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/sidebar/toggle.tsx
@@ -1,5 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/user-sidebar";
+import { ArrowLeftFromLine } from "lucide-react";
+
 const Toggle = () => {
-  return <div>Toggle!</div>;
+  const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
+
+  const label = collapsed ? "Expand" : "Collapse";
+
+  return (
+    <>
+      {!collapsed && (
+        <div className="p-3 pl-6 mb-2 flex items-center w-full ">
+          <p className="font-semibold text-primary">For your</p>
+          <Button>
+            <ArrowLeftFromLine />
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Toggle;
