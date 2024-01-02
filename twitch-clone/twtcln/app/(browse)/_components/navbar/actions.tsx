2fb97c7ee74a31a06cc49d385a5c769815b8253a
Parv-gugnani <9765.parv@gmail.com>
2024-01-02 12:08:34 +0530

    dashbaord

--- twitch-clone/twtcln/app/(browse)/_components/navbar/actions.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/navbar/actions.tsx
@@ -1,3 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton, currentUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { Clapperboard } from "lucide-react";
+
 export const Actions = async () => {
-  return <div>Actions!</div>;
+  const user = await currentUser();
+
+  //
+  return (
+    <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-2">
+      {!user && (
+        <SignInButton>
+          <Button>Login</Button>
+        </SignInButton>
+      )}
+      {/* user  */}
+      {!!user && (
+        <div className="flex items-center gap-x-4">
+          <Button
+            size="sm"
+            variant="ghost"
+            className="text-muted-foreground hover:text-primary"
+            asChild
+          >
+            <Link href={`/u/${user.username}`}>
+              <Clapperboard />
+              <span>DashBoard</span>
+            </Link>
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
