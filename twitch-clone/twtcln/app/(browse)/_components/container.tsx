e3091c2257a59929ffbeae0a63d306ebd580c62e
Parv-gugnani <9765.parv@gmail.com>
2024-01-03 12:53:50 +0530

    Sidebar Completed

--- twitch-clone/twtcln/app/(browse)/_components/container.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/container.tsx
@@ -2,12 +2,23 @@
 
 import { useSidebar } from "@/store/user-sidebar";
 import { cn } from "@/lib/utils";
-
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 interface ContainerProps {
   children: React.ReactNode;
 }
 export const Container = ({ children }: ContainerProps) => {
-  const { collapsed } = useSidebar((state) => state);
+  const matches = useMediaQuery("(max-width:1024px)");
+  const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpand();
+    }
+    //
+  }, [matches, onCollapse, onExpand]);
 
   return (
     <div
