32e193e14c0ec1c1a72d437cc7345a29729ea866
Parv-gugnani <9765.parv@gmail.com>
2024-01-03 12:26:49 +0530

    container

--- twitch-clone/twtcln/app/(browse)/_components/container.tsx
+++ twitch-clone/twtcln/app/(browse)/_components/container.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import { useSidebar } from "@/store/user-sidebar";
+import { cn } from "@/lib/utils";
+
 interface ContainerProps {
   children: React.ReactNode;
 }
 export const Container = ({ children }: ContainerProps) => {
+  const { collapsed } = useSidebar((state) => state);
+
   return (
-    <div>
-      <h1>dddd</h1>
+    <div className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70] lg:ml-60")}>
+      {children}
     </div>
   );
 };
