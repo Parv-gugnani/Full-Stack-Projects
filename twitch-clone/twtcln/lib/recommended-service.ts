9a9236eea04a9f198c8662fcbef9fb73cf56fc3a
Parv-gugnani <9765.parv@gmail.com>
2024-01-04 12:29:06 +0530

    follow service

--- twitch-clone/twtcln/lib/recommended-service.ts
+++ twitch-clone/twtcln/lib/recommended-service.ts
@@ -1,10 +1,36 @@
 import { db } from "./db";
 import { getSelf } from "./auth-service";
 export const getRecommended = async () => {
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  //
+  let userId;
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    //
+    // else
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
   return users;
 };
