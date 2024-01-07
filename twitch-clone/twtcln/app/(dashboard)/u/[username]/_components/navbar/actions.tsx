f48ca1721fcef0ded2ec08bb661b91542b0f510d
Parv-gugnani <9765.parv@gmail.com>
2024-01-07 11:22:57 +0530

    exit

--- twitch-clone/twtcln/app/(dashboard)/u/[username]/_components/navbar/actions.tsx
+++ twitch-clone/twtcln/app/(dashboard)/u/[username]/_components/navbar/actions.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
-import { Clapperboard } from "lucide-react";
-import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 
 export const Actions = () => {
   return (
     <div className="flex items-center justify-end gap-x-2">
-      <Button>
+      <Button
+        size="sm"
+        variant="ghost"
+        className="text-muted-foreground hover:text-primary"
+        asChild
+      >
         <Link href="/">
-          <LogOut />
+          <LogOut className="h-5 w-5 mr-2" />
           Exit
         </Link>
       </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
