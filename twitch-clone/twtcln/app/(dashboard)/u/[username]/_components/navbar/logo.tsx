5f00989e24655b20c329aee65688306ad36002e1
Parv-gugnani <9765.parv@gmail.com>
2024-01-07 10:34:49 +0530

    logout

--- twitch-clone/twtcln/app/(dashboard)/u/[username]/_components/navbar/logo.tsx
+++ twitch-clone/twtcln/app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -14,11 +14,11 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
         <div className="rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image src="/navlogo.png" alt="Gamehub" height="35" width="35" />
+          <Image src="/setting.svg" alt="Gamehub" height="35" width="35" />
         </div>
         <div className={cn("hidden lg:block", font.className)}>
-          <p className="text-lg font-semibold">Gamehub</p>
-          <p className="text-xs text-muted-foreground">Let&apos;s play</p>
+          <p className="text-lg font-semibold">Twitch Clone</p>
+          <p className="text-xs text-muted-foreground">Creator Dashboard</p>
         </div>
       </div>
     </Link>
