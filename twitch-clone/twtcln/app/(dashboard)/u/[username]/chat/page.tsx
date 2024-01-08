9adae0b4561e8c7a96bbb635c1834d7964b17d76
Parv-gugnani <9765.parv@gmail.com>
2024-01-08 12:34:32 +0530

    stream service

--- twitch-clone/twtcln/app/(dashboard)/u/[username]/chat/page.tsx
+++ twitch-clone/twtcln/app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,5 +1,11 @@
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">Chat Setting</h1>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
