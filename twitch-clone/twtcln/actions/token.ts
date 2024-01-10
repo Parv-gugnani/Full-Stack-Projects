6b3b56459f8b9cba72775361ff3595a8654f91e3
Parv-gugnani <9765.parv@gmail.com>
2024-01-10 15:53:54 +0530

    stream

--- twitch-clone/twtcln/actions/token.ts
+++ twitch-clone/twtcln/actions/token.ts
@@ -34,6 +34,19 @@ export const createViewerToken = async (hostIdentity: string) => {
 
   const token = new AccessToken(
     process.env.LIVEKIT_API_KEY!,
-    process.env.LIVEKIT_API_SECRET!
+    process.env.LIVEKIT_API_SECRET!,
+    {
+      identity: isHost ? `host-${self.id}` : self.id,
+      name: self.username,
+    }
   );
+
+  token.addGrant({
+    room: host.id,
+    roomJoin: true,
+    canPublish: false,
+    canPublishData: true,
+  });
+
+  return await Promise.resolve(token.toJwt());
 };
--- twitch-clone/twtcln/components/stream-player.tsx
+++ twitch-clone/twtcln/components/stream-player.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -13,5 +15,22 @@ export const StreamPlayer = ({
   stream,
   isFollowing,
 }: StreamPlayerProps) => {
-  return <div>Stream Player</div>;
+  const { token, name, identity } = useViewerToken(user.id);
+
+  console.log({ token, name, identity });
+
+  if (!token || !name || !identity) return <div>Cannot Watch The Stream</div>;
+
+  //
+  //
+  return (
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+      >
+        Stream
+      </LiveKitRoom>
+    </>
+  );
 };
