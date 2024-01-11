6e8a466ebd74c10698724254428fb06b0be63e3e
Parv-gugnani <9765.parv@gmail.com>
2024-01-11 12:39:04 +0530

    video comp

--- twitch-clone/twtcln/components/stream-player.tsx
+++ twitch-clone/twtcln/components/stream-player.tsx
@@ -28,8 +28,11 @@ export const StreamPlayer = ({
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
       >
-        Stream
+        <div className="sapce-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
       </LiveKitRoom>
     </>
   );
