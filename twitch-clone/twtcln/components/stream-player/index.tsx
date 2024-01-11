8fcea8250b9840cedb5804dd3aedd016d2bab4fd
Parv-gugnani <9765.parv@gmail.com>
2024-01-11 16:13:36 +0530

    full screen

--- twitch-clone/twtcln/components/stream-player/index.tsx
+++ twitch-clone/twtcln/components/stream-player/index.tsx
@@ -1,14 +1,36 @@
 "use client";
 
-import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
-import { Video } from "./video";
+
 import { cn } from "@/lib/utils";
+import { useViewerToken } from "@/hooks/use-viewer-token";
+import { Video } from "./video";
+import { LiveVideo } from "./live-video";
+import { FullscreenControl } from "./fullscreen-control";
+
+type CustomStream = {
+  id: string;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+  isLive: boolean;
+  thumbnailUrl: string | null;
+  name: string;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  stream: CustomStream | null;
+  imageUrl: string;
+  _count: { followedBy: number };
+};
 
 interface StreamPlayerProps {
-  user: User & { stream: Stream | null };
-  stream: Stream;
+  user: CustomUser;
+  stream: CustomStream;
   isFollowing: boolean;
 }
 
@@ -19,25 +41,30 @@ export const StreamPlayer = ({
 }: StreamPlayerProps) => {
   const { token, name, identity } = useViewerToken(user.id);
 
-  console.log({ token, name, identity });
+  if (!token || !name || !identity) {
+    return <StreamPlayerSkeleton />;
+  }
 
-  if (!token || !name || !identity) return <div>Cannot Watch The Stream</div>;
-
-  //
-  //
   return (
-    <>
-      <LiveKitRoom
-        token={token}
-        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className={cn(
-          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
-        )}
-      >
-        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
-          <Video hostName={user.username} hostIdentity={user.id} />
-        </div>
-      </LiveKitRoom>
-    </>
+    <LiveKitRoom
+      token={token}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+      className={cn(
+        "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      )}
+    >
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <Video hostName={user.username} hostIdentity={user.id} />
+      </div>
+    </LiveKitRoom>
+  );
+};
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10"></div>
+      <div className="col-span-1 bg-background"></div>
+    </div>
   );
 };
--- twitch-clone/twtcln/components/stream-player/live-video.tsx
+++ twitch-clone/twtcln/components/stream-player/live-video.tsx
@@ -4,6 +4,7 @@ import { useRef, useState, useEffect } from "react";
 import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
 import { useEventListener } from "usehooks-ts";
+import { FullscreenControl } from "./fullscreen-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -12,6 +13,18 @@ interface LiveVideoProps {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  //
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+    } else if (wrapperRef) {
+      wrapperRef.current?.requestFullscreen();
+      setIsFullscreen(true);
+    }
+  };
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -26,6 +39,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <FullscreenControl
+            isFullscreen={isFullscreen}
+            onToggle={toggleFullscreen}
+          />
+        </div>
+      </div>
     </div>
   );
 };
