e51c2e7e08f834f332581ca340c917866d35e100
Parv-gugnani <9765.parv@gmail.com>
2024-01-11 16:39:34 +0530

    video comp completed

--- twitch-clone/twtcln/components/stream-player/live-video.tsx
+++ twitch-clone/twtcln/components/stream-player/live-video.tsx
@@ -16,6 +16,30 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   const [isFullscreen, setIsFullscreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+
+    setVolume(isMuted ? 50 : 0);
+
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = !isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, [0]);
   //
   const toggleFullscreen = () => {
     if (isFullscreen) {
@@ -47,7 +71,11 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
-          <VolumeControl onChange={() => {}} value={0} onToggle={() => {}} />
+          <VolumeControl
+            onChange={onVolumeChange}
+            value={volume}
+            onToggle={toggleMute}
+          />
           <FullscreenControl
             isFullscreen={isFullscreen}
             onToggle={toggleFullscreen}
