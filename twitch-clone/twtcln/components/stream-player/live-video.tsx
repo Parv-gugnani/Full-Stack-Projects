a630b3da5657157783466b5a19f281168aaa58a6
Parv-gugnani <9765.parv@gmail.com>
2024-01-11 16:31:29 +0530

    vol slider

--- twitch-clone/twtcln/components/stream-player/live-video.tsx
+++ twitch-clone/twtcln/components/stream-player/live-video.tsx
@@ -5,6 +5,7 @@ import { Participant, Track } from "livekit-client";
 import { useTracks } from "@livekit/components-react";
 import { useEventListener } from "usehooks-ts";
 import { FullscreenControl } from "./fullscreen-control";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -19,12 +20,17 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const toggleFullscreen = () => {
     if (isFullscreen) {
       document.exitFullscreen();
-      setIsFullscreen(false);
     } else if (wrapperRef) {
       wrapperRef.current?.requestFullscreen();
-      setIsFullscreen(true);
     }
   };
+
+  const handleFullscreenChange = () => {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  };
+
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
   useTracks([Track.Source.Camera, Track.Source.Microphone])
     .filter((track) => track.participant.identity === participant.identity)
@@ -41,6 +47,7 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <VolumeControl onChange={() => {}} value={0} onToggle={() => {}} />
           <FullscreenControl
             isFullscreen={isFullscreen}
             onToggle={toggleFullscreen}
