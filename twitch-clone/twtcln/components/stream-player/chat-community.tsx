631cf753e9ee1809ff85d70909aa732faa36763e
Parv-gugnani <9765.parv@gmail.com>
2024-01-15 12:12:27 +0530

    no result

--- twitch-clone/twtcln/components/stream-player/chat-community.tsx
+++ twitch-clone/twtcln/components/stream-player/chat-community.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { useParticipants } from "@livekit/components-react";
+import { useState } from "react";
+import { useDebounce } from "usehooks-ts";
+import { ScrollArea } from "../ui/scroll-area";
+import { Input } from "../ui/input";
 
 interface ChatCommunityProps {
   hostName: string;
@@ -14,6 +18,33 @@ export const ChatCommunity = ({
   isHidden,
 }: ChatCommunityProps) => {
   const participants = useParticipants();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce<string>(value, 500);
 
-  return <div></div>;
+  const onChange = (newValue: string) => {
+    setValue(value);
+  };
+
+  if (isHidden) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-sm text-muted-foreground">Community Is Disabled</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4">
+      <Input
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search Community"
+        className="border-white/10"
+      />
+      <ScrollArea className="gap-y-2 mt-4">
+        <p className="text-center text-sm text-muted-foreground hidden last:block">
+          No Results
+        </p>
+      </ScrollArea>
+    </div>
+  );
 };
