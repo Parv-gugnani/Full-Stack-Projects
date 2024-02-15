810a9dd1daa35fb0153f7f65c3061417976768ce
Parv-gugnani <9765.parv@gmail.com>
2024-02-15 16:14:54 +0530

    routing

--- erascript/app/(dashboard)/layout.tsx
+++ erascript/app/(dashboard)/layout.tsx
@@ -1,24 +1,26 @@
-"use client";
-
-import { useOrganization } from "@clerk/nextjs";
-import React from "react";
+import { Sidebar } from "./_components/sidebar";
+import { Navbar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-const DashboardPage = ({ children }: DashboardLayoutProps) => {
-  const { organization } = useOrganization();
-
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl[60px] h-full">
+      <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrgSidebar />
-          <div>{children}</div>
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </div>
     </main>
   );
 };
+
+export default DashboardLayout;
--- erascript/app/(dashboard)/page.tsx
+++ erascript/app/(dashboard)/page.tsx
@@ -15,11 +15,11 @@ const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       this is a organization
-      {/* {!organization ? (
+      {!organization ? (
         <EmptyOrg />
       ) : (
         <BoardList orgId={organization.id} query={searchParams} />
-      )} */}
+      )}
     </div>
   );
 };
