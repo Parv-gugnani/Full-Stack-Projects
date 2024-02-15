b51b13a783283fa4de794d0a243383ccda54af40
Parv-gugnani <9765.parv@gmail.com>
2024-02-15 15:40:43 +0530

    comps

---
+++ erascript/app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+
+export const NewButton = () => {
+  return <Dialog>Dialogtr</Dialog>;
+};
